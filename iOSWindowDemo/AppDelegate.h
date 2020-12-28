d94cc6208030abe0c6ffe4a8af8daee2e9bb165f
ASSASSINYM <darlingiloveu@sina.com>
2020-12-28 14:27:15 +0800

    Initial Commit

---
+++ iOSWindowDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iOSWindowDemo
+//
+//  Created by ASSASSIN on 2020/12/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
